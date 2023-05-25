5b16893799043175f463754601f44f3974544670
adriangonc <adriangonc@yahoo.com.br>
2023-05-25 15:36:25 -0300

    Criação da entidade feature boolean

--- src/main/java/com/feature/service/models/FeatureBoolean.java
+++ src/main/java/com/feature/service/models/FeatureBoolean.java
@@ -1,4 +1,54 @@
 package com.feature.service.models;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.UUID;
+
+@Document(collation = "feature_booleans")
 public class FeatureBoolean {
+
+    @Id
+    private String id;
+    private String name;
+    private boolean active;
+
+    public FeatureBoolean(){
+        this.id = getFeatureId();
+    }
+
+    private static String getFeatureId() {
+        return "FEATURE_" + UUID.randomUUID().toString();
+    }
+
+
+    public FeatureBoolean(String id, String name, boolean active) {
+        this.id = getFeatureId();
+        this.name = name;
+        this.active = active;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
 }
