fa3a7e45808c00bd015671be0f76691aaf06dcba
adriangonc <adriangonc@yahoo.com.br>
2023-06-06 18:33:56 -0300

    Ajustando retorno de erro

--- src/main/java/com/feature/service/controller/FeatureBooleanControler.java
+++ src/main/java/com/feature/service/controller/FeatureBooleanControler.java
@@ -3,6 +3,8 @@ package com.feature.service.controller;
 import com.feature.service.models.dto.FeatureBooleanRecord;
 import com.feature.service.service.FeatureService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
@@ -22,8 +24,15 @@ public class FeatureBooleanControler {
 
     @PostMapping
     @Transactional
-    public Mono<Object> createFeature(@RequestBody FeatureBooleanRecord featureBoolean){
-        return featureService.createFeature(featureBoolean);
+    public Mono<ResponseEntity<Object>> createFeature(@RequestBody FeatureBooleanRecord featureBoolean){
+        return featureService.createFeature(featureBoolean).map(feature -> ResponseEntity.ok(feature))
+                .onErrorResume( error -> {
+                    if(error instanceof Exception){
+                        return Mono.just(ResponseEntity.badRequest().body(error.getMessage()));
+                    } else {
+                        return Mono.just(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build());
+                    }
+                });
     }
 
 }
