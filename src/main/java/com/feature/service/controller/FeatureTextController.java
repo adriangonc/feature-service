307851c4ea31fd2b3bef1f50fbcddcd5aed180f1
adriangonc <adriangonc@yahoo.com.br>
2023-06-12 17:21:41 -0300

    Creação de model para feature text

---
+++ src/main/java/com/feature/service/controller/FeatureTextController.java
@@ -0,0 +1,10 @@
+package com.feature.service.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("feature/text")
+public class FeatureTextController {
+
+}
---
+++ src/main/java/com/feature/service/models/FeatureText.java
@@ -0,0 +1,75 @@
+package com.feature.service.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.UUID;
+
+@Document(collection = "feature_text")
+public class FeatureText {
+    @Id
+    private String id;
+
+    private String userId;
+
+    private String name;
+
+    private boolean active;
+
+    private String value;
+
+    public FeatureText(){
+        this.id = getFeatureId();
+    }
+
+    private static String getFeatureId() {
+        return "FEATURE_" + UUID.randomUUID().toString();
+    }
+
+    public FeatureText(String name, boolean active, String value) {
+        this.id = getFeatureId();
+        this.name = name;
+        this.active = active;
+        this.value = value;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
