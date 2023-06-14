4a1691b63723609463bb15e13c9d0f0faf2aae6c
adriangonc <adriangonc@yahoo.com.br>
2023-06-14 10:19:07 -0300

    Adicionando validações para cadastro

--- src/main/java/com/feature/service/models/FeatureBoolean.java
+++ src/main/java/com/feature/service/models/FeatureBoolean.java
@@ -3,6 +3,9 @@ package com.feature.service.models;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.util.UUID;
 
 @Document(collection = "feature_booleans")
@@ -10,7 +13,14 @@ public class FeatureBoolean {
 
     @Id
     private String id;
+
+    @NotNull
+    @NotEmpty(message = "Field name can't be empty or null!")
+    @Min(value = 5, message = "Field name can't be lower than 5 characters")
     private String name;
+
+    @NotNull
+    @NotEmpty(message = "Field active can't be empty or null!")
     private boolean active;
 
     public FeatureBoolean() {
